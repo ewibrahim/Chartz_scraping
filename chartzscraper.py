e905d335e5470e2ee59861ac6f86c5e2a85bd99f
ewibrahim <73498888+ewibrahim@users.noreply.github.com>
2021-04-26 17:15:18 +0200

    Deuxieme version du scraper
    Scraping fait sur les 100 dernières pages du site de la snep

--- chartzscraper.py
+++ chartzscraper.py
@@ -2,23 +2,17 @@ import scrapy
 
 class ChartzScraper(scrapy.Spider):
     name = 'snep'
-    start_urls = ['https://snepmusique.com/les-certifications']
+    lien = 'https://snepmusique.com/les-certifications/page/'
+    for i in range(100):
+        lienConcat = lien+str(i)
+        start_urls = [lienConcat]
 
-    def parse(self, response):
-        for certifications in response.css('div.certification'):
-            yield {
-                'artiste': certifications.css('div.artiste::text').get(),
-                'titre': certifications.css('div.titre::text').get(),
-                'categorie': certifications.css('div.categorie::text').get(),
-                'editeur': certifications.css('div.editeur::text').get(),
-                'certification': certifications.css('div.certif::text').get(),
-            }
-
-#        pour lancer le scraper sur Scrapy:
-#        "scrapy runspider chartzscraper.py -O nomfichier.json" (ou .csv)
-#
-#        /!\ en attente d'une réponse du professeur
-#
-#        next_page = response.css('div.next').attrib['href']
-#        if next_page is not None:
-#            yield response.follow(next_page, callback=self.parse)
+        def parse(self, response):
+            for certifications in response.css('div.certification'):
+                yield {
+                    'artiste': certifications.css('div.artiste::text').get(),
+                    'titre': certifications.css('div.titre::text').get(),
+                    'categorie': certifications.css('div.categorie::text').get(),
+                    'editeur': certifications.css('div.editeur::text').get(),
+                    'certification': certifications.css('div.certif::text').get(),
+                }
